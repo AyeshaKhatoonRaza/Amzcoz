7568f23f501cbeeed7dc8ca7e395c024284aa582
MohanPrasathSece <mohanprasath.s2023ece@sece.ac.in>
2025-12-15 10:48:42 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import './light-theme.css'
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
